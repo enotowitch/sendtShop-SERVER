1221e61a2bb9a87400c38ca91bbecd8924285ce8
enotowitch <en.enotowitch@gmail.com>
2023-07-25 19:50:06 +0300

    +addOrder(NEW:userId)

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -62,13 +62,13 @@ export const addOrder = async (req, res) => {
 
 	// verify orderToken
 	const { token } = req.body
-	const { cart, shipping, email } = req.userInfo // !!
+	const { cart, shipping, email, _id } = req.userInfo // !!
 
 	const isVerifiedToken = await verifyToken(token)
 	if (isVerifiedToken) { // TODO: prevent dup orders on page reload if link copied
 		if (cart && shipping) {
 			// if user cart on client loaded create order with order.cart(same as user's for easy show prods to admin, as prods are shown in user's cart)
-			const doc = await new order({ cart, shipping, email })
+			const doc = await new order({ cart, shipping, email, userId: _id.toString() })
 			await doc.save()
 			res.json({ ok: true }) // !! wait till order is created
 		}
