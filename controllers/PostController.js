84ec5626c0cb4410b3f231a87ca1c91fc9de39bf
enotowitch <en.enotowitch@gmail.com>
2023-08-02 18:28:57 +0300

    +sortPosts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -36,12 +36,15 @@ export const getAllPosts = async (req, res) => {
 	res.json(response) // all product/article/comment/review... || product.tags/article.likes...
 }
 
+// ! sortPosts
 export const sortPosts = async (req, res) => {
 
 	// type=product/article/comment/review...
 	// field=price/title/...
 	// sortType=asc/desc/...
-	const { type, sortField, sortType } = req.body
+	const { type, sortPostsQuery } = req.body
+	const sortField = sortPostsQuery?.match(/(.+)&(.+)/)?.[1] // eg: price => price&asc
+	const sortType = sortPostsQuery?.match(/(.+)&(.+)/)?.[2] // eg: asc => price&asc
 
 	const sorted = await eval(type).find({}).sort({ [sortField]: sortType })
 	res.json(sorted)
